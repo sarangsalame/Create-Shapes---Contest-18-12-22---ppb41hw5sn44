0444286a108a4e0c4eae62e5ba83ec62e168b4b5
sarang salame <115799065+sarangsalame@users.noreply.github.com>
2022-12-18 20:08:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import '../styles/App.css';
 const App = () => {
+  let [data, SetData]=UseState("Square")
+  let [net, SetNet] = UseState(true)
+const createShapeopt=(e)=>{
+  SetData(e.target.value)
+}
 
+const create=()=>{
+  if(data==="Circle"){
+  return SetNet(false)
+  }
+  else{
+  return SetNet(true)
+}}
   return (
     <div id="main">
       <div id="shape-creator">
-        
+        <select onChange={createShapeopt} value={data}>
+          <option  value="Square">Square</option>
+          <option  value="Circle">Circle</option>
+        </select>
+    <button onCick={create}>Add shape</button>
 
       </div>
       <div id="shapes-holder">
+        <div className={net ? "square" : "circle" }
       </div>
     </div>
   )
